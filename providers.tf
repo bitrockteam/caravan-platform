25acd9af8ccea9aff64a0067a35a89154b46840d
efbar <17676301+efbar@users.noreply.github.com>
2020-07-07 12:59:05 +0200

    add consul provider

--- providers.tf
+++ providers.tf
@@ -2,3 +2,8 @@ provider "vault" {
   address         = var.vault_endpoint
   skip_tls_verify = var.vault_skip_tls_verify
 }
+
+provider "consul" {
+  address    = var.consul_endpoint
+  token      = data.vault_generic_secret.consul_bootstrap_token.data["secretid"]
+}
