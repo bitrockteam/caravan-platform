b4ecea0e406fdf59965b217efbfb45957d41cf8d
efbar <17676301+efbar@users.noreply.github.com>
2020-07-07 12:16:19 +0200

    add consul_endpoint variable

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "vault_endpoint" {
   type    = string
   default = null
 }
+variable "consul_endpoint" {
+  type    = string
+  default = null
+}
 variable "vault_skip_tls_verify" {
   type    = bool
   default = false
