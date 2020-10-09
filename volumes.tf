2e1e6ffc87b38f3114c6beb3c1c52c0060ca1575
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2020-10-09 11:55:22 +0200

    csi volumes

--- volumes.tf
+++ volumes.tf
@@ -1,10 +1,12 @@
-resource "google_compute_disk" "jenkins_master" {
-  count = var.gcp_csi ? 1 : 0
-  name  = "jenkins-master"
-  type  = "pd-ssd"
-  size  = 30
+resource "google_compute_region_disk" "jenkins_master" {
+  count  = var.gcp_csi ? 1 : 0
+  name   = "jenkins-master"
+  type   = "pd-ssd"
+  region = var.gcp_region
+  size   = 30
   labels = {
-    Platform    = "Nomad",
-    Application = "Jenkins-Master"
+    platform = "nomad",
+    job      = "jenkins-master"
   }
+  replica_zones = ["${var.gcp_region}-a", "${var.gcp_region}-b"]
 }
